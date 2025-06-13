cef63c83e5b661e54675d5942679fb54ccf5eddb
Yash Saini <sainiyash1605@gmail.com>
2025-06-13 06:47:39 +0000

    Changes Made

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello Yash from Azure Web App running Python 3.13!"
+    return "Yo! i made a change"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000)
